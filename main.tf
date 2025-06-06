d06d313dd9befa51796598ff7488f92c8a9c32ff
filodeca <165920102+filodeca@users.noreply.github.com>
2025-06-06 16:35:58 +0100

    commit

--- main.tf
+++ main.tf
@@ -9,17 +9,18 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "techcode-webapp_image" {
+resource "docker_image" "retail_app_image" {
   name = "techcode-webapp:latest"
   build {
-    context    = "../"   # Path to Dockerfile relative to terraform folder
-    dockerfile = "../Dockerfile"
+    context    = "./"   # Path to Dockerfile relative to terraform folder
+    dockerfile = "Dockerfile"
   }
 }
 
-resource "docker_container" "techcode-webapp" {
-  image = docker_image.techcode-webapp_image.latest
-  name  = "techcode-webapp_container"
+resource "docker_container" "retail_app" {
+  name  = "retail_app_container"
+  image = docker_image.retail_app_image.name 
+
   ports {
     internal = 3000
     external = 3000
